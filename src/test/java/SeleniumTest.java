6e75b6d580ac78677db812ea971ded071d1bb2d9
elmurodzoda@GMAIL.com <firuz@Firuzs-MacBook-Pro.local>
2020-07-07 18:40:05 -0700

    first commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,20 @@
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    //Testing xml file from Maven
+    //How to execute all the test cases from Test folder - mvn test
+    @Test
+    public void BrowserAutomation()
+    {
+        System.out.println("BrowserAutomation");
+    }
+
+    @Test
+    public void elementsUI()
+    {
+        System.out.println("elementsUI");
+    }
+
+}
